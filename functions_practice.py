9628c693a683badf372ff6c9c523c574c1e21a50
AB27-source <angadbhatti18@gmail.com>
2022-06-16 23:08:35 -0700

    Complete: added functionality for a single list or list greater than 3

--- functions_practice.py
+++ functions_practice.py
@@ -1,4 +1,5 @@
 def hello():
+    print("Greetings!\n")
     return
 
 def pack(arg1, arg2, arg3):
@@ -8,24 +9,37 @@ def pack(arg1, arg2, arg3):
 
 def eat_lunch(listVar):
     if len(listVar) == 0:
-        print("My lunchbox is empty!")
+        print("My lunchbox is empty!\n")
+        return
+    
     else:
-        print("First I eat %s\nNext I eat %s\nLastly I eat %s\n" %(listVar[0], listVar[1], listVar[2]))
+        for i in range(len(listVar)):
+            if i == 0:
+                print("%d. First I eat %s\n" %(i+1, listVar[0]))
+            else:
+                print("%d. Next I eat %s\n" %(i+1, listVar[i]))
+    return
 
 # Calling hello function, does not return anything
+print("\nHello Function:\n----------------------------------")
 hello()
 
-# printing new line
-print("\n")
 
 # Example of running eat_lunch with empty list
-print("Passing an empty list to function:\n----------------------------------")
+print("\nPassing an empty list to function:\n----------------------------------")
 listVar = []
 eat_lunch(listVar)
 
-# printing new line
-print("\n")
 
 # Example of running eat_lunch with pack function
-print("Passing pack function to function:\n----------------------------------")
+print("\nPassing pack function to function:\n----------------------------------")
 eat_lunch(pack("Apple", "Sandwhich", "Chips"))
+
+
+# Example of running eat_lunch with only 1 list item
+print("\nPassing list of 1 item to function:\n-----------------------------------")
+eat_lunch(["Cake"])
+
+# Example of running eat_lunch with only 5 list item
+print("\nPassing list of 5 item to function:\n-----------------------------------")
+eat_lunch(["Cake", "Apple", "Cookie", "Sandwhich", "Chips"])
